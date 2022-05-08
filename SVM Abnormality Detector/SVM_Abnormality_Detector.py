f8c0b2ae3274c1012eb2d6e69ac1893e1f238374
michaelmurray2022 <michael.murray8952@gmail.com>
2022-05-08 08:18:34 -0400

    Cleanup

--- SVM Abnormality Detector/SVM_Abnormality_Detector.py
+++ SVM Abnormality Detector/SVM_Abnormality_Detector.py
@@ -1,5 +1,3 @@
-#https://towardsdatascience.com/support-vector-machine-svm-for-anomaly-detection-73a8d676c331
-
 import pandas as pd
 from sklearn.svm import OneClassSVM
 import matplotlib.pyplot as plt
@@ -9,7 +7,6 @@ df = pd.read_csv("xyz_data.csv")
 
 num_outliers = []
 gamma_val = []
-#gamma_val = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 20.0, 30.0, 40.0]
 for gamm in range(1,1000000,1000):
     gamma_to_use = gamm*0.0001
     model2 = OneClassSVM(kernel='rbf', gamma = gamma_to_use, nu = 0.03).fit(df)
@@ -29,8 +26,7 @@ min_idx = num_outliers.index(min)
 
 
 #speficy the model
-model = OneClassSVM(kernel='rbf', gamma = gamma_val[min_idx], nu = 0.03).fit(df) #was 0.001 and 0.03
-
+model = OneClassSVM(kernel='rbf', gamma = gamma_val[min_idx], nu = 0.03).fit(df)
 
 #prediction
 y_pred = model.predict(df)
@@ -40,13 +36,6 @@ outlier_index = where(y_pred==-1)
 outlier_values = df.iloc[outlier_index]
 for idx in outlier_index:
     df = df.drop(index = idx)
-
-#visualize
-#fig = plt.figure()
-#ax = plt.axes(projection='3d')
-#ax.scatter3D(df["x"], df["y"], df["z"])
-#ax.scatter3D(outlier_values["x"], outlier_values["y"], outlier_values["z"], c ="r")
-#plt.show()
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
